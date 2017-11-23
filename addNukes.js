9eec64a3cff68a939881def93d486ea1624c05c3
garyblasco <garyblasco@gmail.com>
2017-11-23 11:51:10 -0500

    Add files via upload

---
+++ addNukes.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-type': 'text/html'});
+	var q = url.parse(req.url, true).query;
+	var txt = q.target + ' ' + q.date + ' ' + q.nukes; 
+	res.write('Added! <br> Name: ' + q.target +'<br> Nukes: ' + q.nukes + '<br> Date & Time: ' + q.date + '<br>');
+	res.end('<a href="http://test.com/?target=' + q.target + '&date=' + q.date + '&nukes=' + q.nukes + '">test url</a>');
+}).listen(9999);
